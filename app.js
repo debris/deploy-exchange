62aef38f690115006fc96d6c3949437e4b02fb46
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-05-18 17:49:11 +0200

    contract creation

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
+var Promise = require('bluebird');
+var path = require('path');
+var fs = Promise.promisifyAll(require('fs'));
+var exec = Promise.promisify(require('child_process').exec);
+
 var web3 = require('web3');
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
+var config = 'config.json';
 
+var toString = function (obj) {
+    return obj.toString();
+};
+
+var config = fs.readFileAsync('config.json')
+.then(toString)
+.then(JSON.parse);
+
+var name = 'ClientReceipt';
+
+var readAbi = function () {
+    return fs.readFileAsync(path.join('contracts', name + '.abi')).then(toString).then(JSON.parse);
+};
+
+var readBinary = function () {
+    return fs.readFileAsync(path.join('contracts', name + '.binary')).then(toString);
+};
+
+var ClientReceiptAbi    = exec('cd contracts && solc --inputFile '+ name +'.sol --json-abi file --add-std 1').then(readAbi);
+var ClientReceiptBinary = exec('cd contracts && solc --inputFile '+ name +'.sol --binary file --add-std 1').then(readBinary);
+
+Promise.all([config, ClientReceiptAbi, ClientReceiptBinary]).then(function (arr) {
+    var config = arr[0];
+    var abi = arr[1];
+    var code = arr[2];
+    var namereg = config.namereg === 'default' ? web3.eth.namereg.address : config.namereg;
+    var from = config.from === "coinbase" ? web3.eth.coinbase : config.coinbase;
+    var institution = config.institution;
+    
+    var contract = web3.eth.contract(abi).new(namereg, institution, { from: from, gas: 500000 });
+    return contract.address;
+}).catch(function (err) {
+    console.error(err);
+}).done(function (address) {
+    console.log("CONTRACT ADDRESS: " + address);
+});
 
---
+++ contracts/ClientReceipt.sol
@@ -0,0 +1,70 @@
+import "owned";
+
+// Solidity interface for namereg
+contract NameReg{
+    function register(bytes32 name){}
+}
+
+contract killswitch is owned {
+  event NomineesChanged(address keyholder, address executive);
+  event BoxOpened();
+  event BoxClosed();
+  function nominate(address _keyholder, address _executive) onlyowner {
+  keyholder = _keyholder;
+  executive = _executive;
+  NomineesChanged(keyholder, executive);
+  }
+
+  function open() {
+    if (msg.sender == owner || msg.sender == keyholder) {
+      isOpen = true;
+      BoxOpened();
+    }
+  }
+  function close() {
+    if (msg.sender == owner || msg.sender == keyholder) {
+      isOpen = false;
+      BoxClosed();
+    }
+  }
+  modifier restricted {
+  if (msg.sender == owner || (isOpen && msg.sender == executive)) {
+    _
+  }
+  }
+
+  bool isOpen;
+  address keyholder;
+  address executive;
+}
+
+contract ClientReceipt is owned, killswitch {
+
+  function ClientReceipt(address _namereg, bytes32 _institution) {
+    NameReg(_namereg).register(_institution);
+  }
+
+  event AnonymousDeposit(address indexed _from, uint _value);
+  event Deposit(address indexed _from, bytes32 _id, uint _value);
+  event Refill(address indexed _from, uint _value);
+  event Withdraw(address indexed _from, address indexed _to, uint _value);
+  event Drain(address indexed _from, address indexed _to, uint _value);
+
+  function() {
+    AnonymousDeposit(msg.sender, msg.value);
+  }
+  function deposit(bytes32 _id) {
+    Deposit(msg.sender, _id, msg.value);
+  }
+  function refill() {
+    Refill(msg.sender, msg.value);
+  }
+  function withdraw(address _to, uint _value) restricted {
+    _to.send(_value);
+    Withdraw(msg.sender, _to, _value);
+  }
+  function drain(address _to, uint _value) restricted {
+    _to.send(_value);
+    Drain(msg.sender, _to, _value);
+  }
+}
